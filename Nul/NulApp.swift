f6f9d6fbc27b522d9246d1d97b6bc28e521e2a84
dmueller39 <danielmueller@uwalumni.com>
2025-03-19 08:09:38 -0400

    Add stub xcode project

---
+++ Nul/NulApp.swift
@@ -0,0 +1,17 @@
+//
+//  NulApp.swift
+//  Nul
+//
+//  Created by Daniel Mueller on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
